d629262fce5b6e637c02a5a53f62add092be1982
Glitch (quiver-boat) <none>
2019-01-07 13:27:41 +0000

    🚄😜 Checkpoint ./routes/api.js:2505047/407

--- routes/api.js
+++ routes/api.js
@@ -19,21 +19,24 @@ module.exports = function (app) {
   app.route('/api/stock-prices')
     .get(function (req, res){
         //console.log(req.query);
+    let result = {"stockData": {}};
     if(req.query.stock) {
-      const stock1 = req.query.stock
+      const stock = req.query.stock
       //console.log(stock1);
-      if (stock1){console.log(typeof stock1)}
-      
-    
-      const stock_url = 'https://api.iextrading.com/1.0/stock/'+ stock1 + '/book';
+      let loops = 1;
+      if (typeof stock == 'object' ){loops = 2} 
+      for (let i = 0; i < loops; i ++){ 
+        const stock_url = 'https://api.iextrading.com/1.0/stock/'+ stock[i] + '/book';
 
-      fetch(stock_url)
-      .then(function(response) {
-        return response.json();
-      })
-      .then(function(myJson) {
-        res.json(myJson.quote.latestPrice);
-      });
+        fetch(stock_url)
+        .then(function(response) {
+          return response.json();
+        })
+        .then(function(myJson) {
+          
+          res.json(myJson.quote.latestPrice);
+        });
+      }
     }
   });
   
