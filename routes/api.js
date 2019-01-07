20a1936c874c392266a64d7605721c3f5ef53066
Glitch (quiver-boat) <none>
2019-01-07 13:49:40 +0000

    🏢☠️ Checkpoint ./routes/api.js:2505047/950

--- routes/api.js
+++ routes/api.js
@@ -18,27 +18,27 @@ module.exports = function (app) {
 
   app.route('/api/stock-prices')
     .get(function (req, res){
+      let result = [];
         //console.log(req.query);
-    let result = [];
-    if(req.query.stock) {
-      const stock = req.query.stock
-      //console.log(stock1);
-      let loops = 1;
-      if (typeof stock == 'object' ){loops = 2} 
-      for (let i = 0; i < loops; i ++){ 
-        const stock_url = 'https://api.iextrading.com/1.0/stock/'+ stock[i] + '/book';
+      if(req.query.stock) {
+        const stock = req.query.stock
+        //console.log(stock1);
+        let loops = 1;
+        if (typeof stock == 'object' ){loops = 2} 
+        for (let i = 0; i < loops; i ++){ 
+          const stock_url = 'https://api.iextrading.com/1.0/stock/'+ stock[i] + '/book';
 
-        fetch(stock_url)
-        .then(function(response) {
-          return response.json();
-        })
-        .then(function(price) {
-          result.push({"stock":stock[i],"price":price.quote.latestPrice,"rel_likes":1});
-          console.log(result);
-        });
+          fetch(stock_url)
+          .then(function(response) {
+            return response.json();
+          })
+          .then(function(price) {
+            result.push({"stock":stock[i],"price":price.quote.latestPrice,"rel_likes":1});
+          });
+        }
       }
+      console.log(result);
       res.json(result);
-    }
   });
   
   
