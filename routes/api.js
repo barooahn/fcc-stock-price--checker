8643b2147f4e91c8bb10218a51c9528391d75a83
Glitch (quiver-boat) <none>
2019-01-08 15:26:32 +0000

    🕵🙋 Checkpoint ./routes/api.js:2505047/2907

--- routes/api.js
+++ routes/api.js
@@ -19,40 +19,48 @@ const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRIN
 module.exports = function (app) {
   
   
-  //addlike
-      //check database for userIp
-      //if not there save userIp, stock
-      //if there remove 
+
   
   //getlikes
+    //find stock 
+    //return count of results
+  const getLikes = (stock) => {
+    let result; 
+    MongoClient.connect(CONNECTION_STRING, function(err, db) {
+      const collection = db.collection(project);
+      db.collection.find({
+        query: { stock: stock },
+        function(err,docs){
+          if(err) throw(err)
+          result = docs.count()
+        }  
+      });
+      db.close();
+      return result
+    }); 
+  }
   
-  
-  
-  const likes = (userIp, stock) => {
-        //check database for ip 
-        let result;
+  //addlike
+  const addLike = (userIp, stock) => {
+      //check database for userIp
+      //if not there save userIp, stock
+      //if there update stock
+     
         MongoClient.connect(CONNECTION_STRING, function(err, db) {
             const collection = db.collection(project);
             db.collection.findAndModify({
-              query: { stock: stock },
+              query: { userIp: userIp },
               update: {
                 $setOnInsert: { stock: stock },
-                $push: {userIp: userIp}
               },
               new: true,   // return new doc if one is upserted
               upsert: true}, // insert the document if it does not exist
               function(err,doc){
                 if(err) throw(err)
-                result = doc.userIp.count();
               }  
             );
-        db.close();
-        return result;
-      });
-        //if there update stock liked
-        //if not add stock to db under ip address
-        
-        //query database for stock and return number of results
+            db.close();
+        });
   }
   
   
